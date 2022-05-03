b41e5f9f0c747784fe73ab1226244bca6b291dc7
Ruslan <frostwow13@yandex.ru>
2022-05-03 20:20:22 +0400

    complete logic for multiple numbers

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -4,44 +4,56 @@ import './Wrapper.css';
 import ButtonBox from "../ButtonBox/ButtonBox";
 import Screen from "../Screen/Screen";
 
-let firstNumber = '';
-let secondNumber = '';
-let operand = '';
+let numbers: Array<string> = [];
+let operands: Array<string> = [];
 
 const Wrapper = (): JSX.Element => {
-  const [textToShow, setTextToShow] = useState<string>(' ');
+  const [textToShow, setTextToShow] = useState<string>('');
 
-  function calculate(fNumber: string, sNumber: string, oper: string): number {
-    switch (oper) {
-      case '%':
-        return Number(fNumber) % Number(sNumber);
-      case '/':
-        return Number(fNumber) / Number(sNumber);
-      case 'X':
-        return Number(fNumber) * Number(sNumber);
-      case '-':
-        return Number(fNumber) - Number(sNumber);
-      case '+':
-        return Number(fNumber) + Number(sNumber);
-      default:
-        return 0;
+  function calculate(): any {
+    let result = Number(numbers[0]);
+    console.log(numbers);
+    for (let i = 0; i < operands.length; i += 1) {
+      switch (operands[i]) {
+        case '%':
+          result %= Number(numbers[i + 1]);
+          break;
+        case '/':
+          result /= Number(numbers[i + 1]);
+          break;
+        case 'X':
+          result *= Number(numbers[i + 1]);
+          break;
+        case '-':
+          result -= Number(numbers[i + 1]);
+          break;
+        case '+':
+          result += Number(numbers[i + 1]);
+          break;
+        default:
+          result = 0;
+      }
     }
+    return result;
   }
 
-  const handleClick = useCallback((symbol: number | string): any => {
+  const handleClick = useCallback((symbol: number | string): void => {
     if (Number.isInteger(symbol) || symbol === '.') {
       setTextToShow(t => t + symbol.toString());
     } else if (!Number.isInteger(symbol)) {
       if (symbol.toString() !== '=') {
-        firstNumber += textToShow;
+        if (textToShow) numbers.push(textToShow);
         setTextToShow('');
-        operand = symbol.toString();
+        if (!Number.isNaN(Number(numbers[0]))) operands.push(symbol.toString());
       } else {
-        secondNumber += textToShow;
-        setTextToShow(calculate(firstNumber, secondNumber, operand).toString());
+        numbers.push(textToShow);
+        setTextToShow(calculate().toString());
       }
     }
-
+    if (symbol === 'C') {
+      numbers = [];
+      operands = [];
+    }
   }, [textToShow]);
 
   return (
